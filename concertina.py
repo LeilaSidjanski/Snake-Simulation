4d50172f6190eb5c6be62a6f01b4f70937618b1e
Giovanni Ranieri <gioran2017@gmail.com>
2023-04-08 17:18:16 +0200

    :frog: concertina updated

--- concertina.py
+++ concertina.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.animation import FuncAnimation
-import time
+
 
 num_joints = 120
 joint_length =1
@@ -9,45 +8,48 @@ joint_length =1
 snake_y = np.zeros(num_joints) + 6
 snake_x = (np.arange(num_joints) * joint_length) + 6
 
-
 wavelength = 5
 amplitude = 3.5
 offset = 0
 
-
-def animate_snake(i):
-    plt.clf()
-    plt.plot(snake_x, snake_y, 'ko-', lw=2)
-    plt.xlim([-0.2, joint_length*num_joints+10.2])
-    plt.ylim([-0.2, joint_length*num_joints+10.2])
-    plt.title('Snake Motion')
-
-    sine_wave_simple()
-
-def wavelength_factor(mult_wave_length, final_wave_length, steps):
-    return np.power(steps / (mult_wave_length * final_wave_length), 1 / steps)
+t = np.linspace(0, 2*np.pi, 250)
+freq = 3.0
 
 def amplitude_factor(ampl, stop_ampl, steps):
-    return np.power(ampl / stop_ampl, 1 / steps)
+    return np.power(ampl / stop_ampl, 1 / steps)  
+
+fig, ax = plt.subplots()
+
+ax.set_xlim(0, 100)
+ax.set_ylim(-10, 10)
+
+joints = ax.scatter(snake_x, snake_y)
 
 
-def sine_wave_simple():
-    global snake_x, snake_y
+joints.set_offsets(np.c_[snake_x, snake_y])
+plt.draw()
+plt.pause(2) 
 
-    k = 1.2
+for i in range(len(t)):
     l = 1
-    for j in range(1, num_joints):
-        snake_y[j] = snake_y[j - 1] + offset + l * amplitude * np.sin(k * wavelength * j*2 * np.pi/(num_joints - 1))
-        k /= wavelength_factor(wavelength, 1.1, num_joints - 1)
-        l /= amplitude_factor(amplitude, 0.5, num_joints - 1)  
+    for j in range(num_joints):
+        snake_y[j] = offset + l * amplitude * np.sin(freq * t[i] + wavelength * j*2 * np.pi/(num_joints - 1))
+
+        l /= amplitude_factor(amplitude, 0.5, num_joints - 1)
+
+    joints.set_offsets(np.c_[snake_x, snake_y])
+    plt.draw()
+    plt.pause(0.001)    
 
 
-    for j in range(1, num_joints):
-        snake_y[j] = snake_y[j - 1] + offset + l * amplitude * np.sin(k * wavelength * j*2 * np.pi/(num_joints - 1))
-        k *= wavelength_factor(wavelength, 1.1, num_joints - 1)
-        l *= amplitude_factor(amplitude, 0.5, num_joints - 1)                  
+for i in range(len(t)):
+    l = 0.14
+    for j in range(num_joints):
+        snake_y[j] = offset + l * amplitude * np.sin(freq * t[i] + wavelength * j*2 * np.pi/(num_joints - 1))
 
+        l *= amplitude_factor(amplitude, 0.5, num_joints - 1)    
 
-fig = plt.figure(figsize=(17,17))
-anim = FuncAnimation(fig, animate_snake, interval=50) # 10
-plt.show()
+    joints.set_offsets(np.c_[snake_x, snake_y])
+    plt.draw()
+    plt.pause(0.001)
+    
--- final_snake_length_change.py
+++ final_snake_length_change.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-num_joints = 8
+num_joints = 12
 amplitude = 1.0
+wavelength = 5
 offset = 0
 
 x = np.zeros(num_joints)
@@ -23,7 +24,7 @@ joints = ax.scatter(x, y)
 
 for i in range(len(t)):
     for j in range(num_joints):
-        x[j] = offset + amplitude*np.sin(freq*t[i] + j*2 * np.pi/(num_joints-1))
+        x[j] = offset + amplitude*np.sin(freq*t[i] + wavelength * j*2 * np.pi/(num_joints-1))
 
     joints.set_offsets(np.c_[x, y])
     plt.draw()
